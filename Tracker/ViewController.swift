58889469520125107cbda8771512060ae0b5b981
Vladimir <zonda4242@gmail.com>
2023-10-31 13:54:21 +0100

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Владимир Клевцов on 31.10.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
