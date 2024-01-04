988d9b63f16ecff0dc4ad61c716e60a35e3b3229
Vladimir <zonda4242@gmail.com>
2024-01-04 16:57:01 +0100

    no message

--- Tracker/Stats/StatsCell.swift
+++ Tracker/Stats/StatsCell.swift
@@ -5,4 +5,44 @@
 //  Created by Владимир Клевцов on 4.1.24..
 //
 
-import Foundation
+//import UIKit
+//import SwiftUI
+//
+//final class StatisticCell: UITableViewCell {
+//
+//    // MARK: - Properties
+//
+//    private let gradientLayer = CAGradientLayer()
+//   let gradient = Gradient(colors: [UIColor(named: "gradient1"), UIColor(named: "gradient2"), UIColor(named: "gradient3")])
+//    // MARK: - Initialization
+//
+//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+//        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//        setupGradient()
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//        setupGradient()
+//    }
+//
+//    // MARK: - Setup
+//
+    private func setupGradient() {
+//        let startColor = UIColor(named: "gradient1")?.cgColor
+//            let middleColor = UIColor(named: "gradient2")?.cgColor
+//            let endColor = UIColor(named: "gradient3")?.cgColor
+//
+//        gradientLayer.colors = [startColor, middleColor, endColor]
+//        gradientLayer.frame = contentView.bounds
+//        gradientLayer.cornerRadius = 16
+//        
+//        let shape = CAShapeLayer()
+//        shape.lineWidth = 1
+//        shape.path = UIBezierPath(rect: contentView.bounds).cgPath
+//        shape.strokeColor = UIColor.black.cgColor
+//        shape.fillColor = UIColor.clear.cgColor
+//        gradientLayer.mask = shape
+//        contentView.layer.addSublayer(gradientLayer)
+    }
+//}
--- Tracker/Stats/StatsViewController.swift
+++ Tracker/Stats/StatsViewController.swift
@@ -4,56 +4,110 @@
 //
 //  Created by Владимир Клевцов on 31.10.23..
 //
-
 import UIKit
 
 final class StatsViewController: UIViewController {
     let titleView = UITextView()
-    let tableView = UITableView()
-    
+    let bestPeriodView = UIView()
+
     let statistics = NSLocalizedString("statistics", comment: "")
-    let bestPeriod = NSLocalizedString("bestPeriod", comment: "")
-    let perfectDays = NSLocalizedString("perfectDays", comment: "")
-    let trackersCompleted = NSLocalizedString("trackersCompleted", comment: "")
-    let average = NSLocalizedString("average", comment: "")
 
     override func viewDidLoad() {
+        super.viewDidLoad()
         view.backgroundColor = .white
         setUpView()
-        
     }
+
     func setUpView() {
         titleView.translatesAutoresizingMaskIntoConstraints = false
         titleView.text = statistics
         titleView.font = UIFont.systemFont(ofSize: 32, weight: .bold)
         view.addSubview(titleView)
-        
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.delegate = self
-        tableView.dataSource = self
-        
+
+        bestPeriodView.translatesAutoresizingMaskIntoConstraints = false
+//        bestPeriodView.layer.cornerRadius = 16
+        bestPeriodView.backgroundColor = .gray
+        bestPeriodView.gradientBorder(colors: [UIColor.red , UIColor.blue], isVertical: false)
+        view.addSubview(bestPeriodView)
+
         NSLayoutConstraint.activate([
             titleView.topAnchor.constraint(equalTo: view.topAnchor, constant: 88),
             titleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             titleView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -105),
             titleView.heightAnchor.constraint(equalToConstant: 41),
-            
-            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 206),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            tableView.bottomAnchor.constraint(equalTo: <#T##NSLayoutAnchor<NSLayoutYAxisAnchor>#>)
-            ])
+
+            bestPeriodView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            bestPeriodView.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: 77),
+            bestPeriodView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            bestPeriodView.heightAnchor.constraint(equalToConstant: 90)
+        ])
     }
-    
+
+//    private func setupGradient() {
+//        let gradientLayer = CAGradientLayer()
+//        gradientLayer.frame = bestPeriodView.bounds
+//        gradientLayer.colors = [
+//            UIColor(named: "gradient1")?.cgColor ?? UIColor.red.cgColor,
+//            UIColor(named: "gradient2")?.cgColor ?? UIColor.green.cgColor,
+//            UIColor(named: "gradient3")?.cgColor ?? UIColor.white.cgColor
+//        ]
+//        gradientLayer.cornerRadius = bestPeriodView.layer.cornerRadius
+//        gradientLayer.borderWidth = 1
+//
+//        bestPeriodView.layer.addSublayer(gradientLayer)
+//    }
 }
-extension StatsViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        4
+
+
+
+//private func setupGradient() {
+//    let startColor = UIColor(named: "gradient1")?.cgColor
+//    let middleColor = UIColor(named: "gradient2")?.cgColor
+//    let endColor = UIColor(named: "gradient3")?.cgColor
+//
+//    gradientLayer.colors = [startColor, middleColor, endColor]
+//    gradientLayer.frame = bestPeriodView.bounds
+//    gradientLayer.cornerRadius = 16
+//    gradientLayer.borderWidth = 1
+//    let shape = CAShapeLayer()
+//    shape.lineWidth = 1
+//    shape.path = UIBezierPath(rect: bestPeriodView.bounds).cgPath
+//    shape.strokeColor = UIColor.black.cgColor
+//    shape.fillColor = UIColor.clear.cgColor
+//    gradientLayer.mask = shape
+//    bestPeriodView.layer.addSublayer(gradientLayer)
+//}
+extension UIView {
+    func gradientBorder(colors: [UIColor], isVertical: Bool){
+        self.layer.masksToBounds = true
+        
+        //Create gradient layer
+        let gradient = CAGradientLayer()
+        gradient.frame = CGRect(origin: CGPoint.zero, size: self.bounds.size)
+        gradient.colors = colors.map({ (color) -> CGColor in
+            color.cgColor
+        })
+
+        //Set gradient direction
+        if(isVertical){
+            gradient.startPoint = CGPoint(x: 0.5, y: 0)
+            gradient.endPoint = CGPoint(x: 0.5, y: 1)
+        }
+        else {
+            gradient.startPoint = CGPoint(x: 0, y: 0.5)
+            gradient.endPoint = CGPoint(x: 1, y: 0.5)
+        }
+
+        //Create shape layer
+        let shape = CAShapeLayer()
+        shape.lineWidth = 1
+        shape.path = UIBezierPath(roundedRect: gradient.frame.insetBy(dx: 1, dy: 1), cornerRadius: self.layer.cornerRadius).cgPath
+        shape.strokeColor = UIColor.black.cgColor
+        shape.fillColor = UIColor.clear.cgColor
+        gradient.mask = shape
+
+        //Add layer to view
+        self.layer.addSublayer(gradient)
+        gradient.zPosition = 0
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
-    
 }
